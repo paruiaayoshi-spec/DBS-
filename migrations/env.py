7b7907653f307deef47975f512a4edcd0a2cc9e4
vaibahaviman212 <vaibhaviman212@gmail.com>
2026-09-07 21:40:43 +0530

    T019: Set up Alembic database migration framework with initial configuration

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,6 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from db import db
 
 # this is the Alembic Config object
 config = context.config
